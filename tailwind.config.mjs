c589c64131b69bb07b72ca040cbe04c88424b128
Julien Butty <julienbutty@gmail.com>
2024-02-09 19:48:43 +0100

    fix: add pnpm version in github action

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,4 +1,3 @@
-const defaultTheme = require("tailwindcss/defaultTheme");
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
