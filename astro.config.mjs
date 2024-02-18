fe6d352bf8af5adf3b16cecb87de16c3ee0fe594
Julien Butty <julienbutty@gmail.com>
2024-02-18 11:16:36 +0100

    feat: add "accompagnements" section

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
+
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), icon()],
   site: "https://julienbutty.github.io",
-  base: "/mamie-b",
+  base: "/mamie-b"
 });
